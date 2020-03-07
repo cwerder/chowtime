1fec500e8a865067ee12ddcd54c075602e3d779b
CJ Werder <cwerder15@gmail.com>
2020-03-06 22:01:06 -0500

    Establish local MongoDB instance and programmatically register first user to it

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
-const port = 3000
+require('dotenv').config()
 
 // initialize routing
 var userRouter = require('./routes/user');
@@ -10,6 +11,7 @@ app.use(express.urlencoded({ extended: true })) // for parsing application/x-www
 
 // initialize middleware
 var timeLoggerMiddleware = require('./middleware/TimeLogger');
+app.use(morgan('combined'))
 
 // link middleware
 app.use(timeLoggerMiddleware)
@@ -17,4 +19,36 @@ app.use(timeLoggerMiddleware)
 // link routers
 app.use('/user', userRouter)
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const MongoClient = require('mongodb').MongoClient;
+var mongoose = require('mongoose');
+console.log('env', process.env.ENVIRONMENT);
+const uri = process.env.ENVIRONMENT === 'DEV' ?
+  `localhost:${process.env.DB_PORT_LOCAL}` :
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@chowtimecluster-h0ntn.azure.mongodb.net/test?retryWrites=true&w=majority`;
+console.log('uri', uri);
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected via mongoose!')
+
+  let registerSchema = new mongoose.Schema({
+    email: String,
+    password: String,
+    confirm_password: String
+  });
+
+  let User = mongoose.model('register', registerSchema);
+
+  let user1 = new User({ email: 'a', password: 'x', confirm_password: 'x'});
+
+  user1.save((error, user) => {
+    if (error) {
+      return console.error(error);
+    }
+    console.log('user successfully added:', user);
+  });
+});
+
+app.listen(process.env.APP_PORT, () => console.log(`Example app listening on port ${process.env.APP_PORT}!`))
